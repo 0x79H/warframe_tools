6d9a0e654c1159c00bc6280bbe9b8253a1cb4973
xz <zhegeyouxiangzhendeshijiade@hotmail.com>
2018-03-26 14:37:32 +0800

    :D

--- warframemarket/warframe.py
+++ warframemarket/warframe.py
@@ -1,14 +1,46 @@
 #  coding=utf-8
+# warframe_id:LesterGeralt :D
 import ctypes
 import json
 import urllib2
-from collections import OrderedDict
 from bs4 import BeautifulSoup
 import time
 
 wanna_item = []
 alert = True
-#alert = False
+name_dict = {}
+url_dict = {}
+
+
+# alert = False
+def get_cn_name():
+    # url = "https://warframe.huijiwiki.com/wiki/warframe中文维基:中英名称对照"
+    # url = url.split('://')
+    # url[1] = urllib2.quote(url[1])
+    # handle = urllib2.urlopen(url[0] + '://' + url[1])
+    handle = urllib2.urlopen("https://warframe.huijiwiki.com/wiki/UserDict")
+    bs = BeautifulSoup(handle.read(), "lxml")
+    handle.close()
+    # table = bs.findAll("table")[2]
+    for row in bs.findAll('tr'):
+        # dicts = row.findAll('td')
+        try:
+            # name_dict[dicts[0].string.strip("\n")] = dicts[1].string.strip("\n")
+            name_dict[row.find('th').string] = row.find('td').string.strip("\"")
+
+        except Exception, e:
+            print e
+            print row
+            print "="*10
+
+
+def get_url_name():
+    url = "https://api.warframe.market/v1/items"
+    handle = urllib2.urlopen(url)
+    aa = json.load(handle)
+    for items in aa['payload']['items']['en']:
+        url_dict[items["item_name"]] = items["url_name"]
+
 
 def get_price(items):
     temp = []
@@ -26,96 +58,86 @@ def get_price(items):
     return temp[-1]
 
 
-# copy frome pyperclip
+# copy from pyperclip
 def winSetClipboard(text):
     GMEM_DDESHARE = 0x2000
     ctypes.windll.user32.OpenClipboard(0)
     ctypes.windll.user32.EmptyClipboard()
-    try:
-        # works on Python 2 (bytes() only takes one argument)
-        hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(bytes(text)) + 1)
-    except TypeError:
-        # works on Python 3 (bytes() requires an encoding)
-        hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(bytes(text, 'ascii')) + 1)
+    # works on Python 2 (bytes() only takes one argument)
+    hCd = ctypes.windll.kernel32.GlobalAlloc(GMEM_DDESHARE, len(bytes(text)) + 1)
     pchData = ctypes.windll.kernel32.GlobalLock(hCd)
-    try:
-        # works on Python 2 (bytes() only takes one argument)
-        ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text))
-    except TypeError:
-        # works on Python 3 (bytes() requires an encoding)
-        ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text, 'ascii'))
+    # works on Python 2 (bytes() only takes one argument)
+    ctypes.cdll.msvcrt.strcpy(ctypes.c_char_p(pchData), bytes(text))
     ctypes.windll.kernel32.GlobalUnlock(hCd)
     ctypes.windll.user32.SetClipboardData(1, hCd)
     ctypes.windll.user32.CloseClipboard()
 
 
 def find_good_price(item):
-#    print(u"[%s]===============================================" % item["cn_name"])
-    print("="*10+("%s"%time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))+"="*10)
+    #    print(u"[%s]===============================================" % item["cn_name"])
+    print("=" * 10 + ("%s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "=" * 10)
     try:
         min_order = get_price(item)
     except  Exception:
-        print(u"%s查询出错"%"["+item["cn_name"]+"]")
+        print(u"%s查询出错" % "[" + item["cn_name"] + "]")
         time.sleep(60)
         return
-#    print(u"%s \r\n查询的[%10s]最低价为: %10d" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),item["cn_name"], min_order[u'platinum']))
-    print(u"查询的%10s最低价为: %10d" % ("["+item["cn_name"]+"]", min_order[u'platinum']))
+    # print(u"%s \r\n查询的[%10s]最低价为: %10d" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),item["cn_name"], min_order[u'platinum']))
+    print(u"查询的%10s最低价为: %10d" % ("[" + item["cn_name"] + "]", min_order[u'platinum']))
     chat = u"/w %s Hi! I want to buy: %s for %d platinum. (warframe.market)" % (
         min_order[u'user'][u'ingame_name'], item["name"], min_order[u'platinum'])
     print(chat)
-    if ((alert==True)&(min_order[u'platinum'] < item["min_price"]) &(min_order[u'platinum'] <item["wanna_price"])):
-        ctypes.windll.user32.MessageBoxW(0, u"[%s]当前的最低价格为%d" % (item["cn_name"], min_order[u'platinum']), u"最低价!", 0x00001040L)
+    if ((alert == True) & (min_order[u'platinum'] < item["min_price"]) & (
+            min_order[u'platinum'] < item["wanna_price"])):
+        ctypes.windll.user32.MessageBoxW(0, u"[%s]当前的最低价格为%d" % (item["cn_name"], min_order[u'platinum']), u"最低价!",
+                                         0x00001040L)
         winSetClipboard(chat)
-    if(item["min_price"]<min_order[u'platinum']):
-        with open("D:\\warframe.txt","a") as f:
-            f.write((u"%s%20s                  低价%10d\r"%(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),item["cn_name"],min_order[u'platinum'])).encode('utf-8'))
+    if item["min_price"] < min_order[u'platinum']:
+        with open("D:\\warframe.txt", "a") as f:
+            f.write((u"%s%20s                  低价%10d\r" % (
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), item["cn_name"], min_order[u'platinum'])).encode(
+                'utf-8'))
     item["min_price"] = min_order[u'platinum']
     time.sleep(30)
 
+def add_item(name, cn_name="", web_name="", price=0xFFFFFFFF):
+    if (cn_name != "") & (web_name != ""):
+        wanna_item.append(
+            {"name": name, "cn_name": cn_name.decode('utf-8'), "web_name": web_name, "min_price": 0xFFFFFFFF,
+             "wanna_price": price})
+    else:
+        wanna_item.append(
+            {"name": name, "cn_name": name_dict[name], "web_name": url_dict[name], "min_price": 0xFFFFFFFF,
+             "wanna_price": price})
 
-def get_name():
-    res2 = urllib2.urlopen(
-        "https://warframe.huijiwiki.com/wiki/warframe%E4%B8%AD%E6%96%87%E7%BB%B4%E5%9F%BA:%E4%B8%AD%E8%8B%B1%E5%90%8D%E7%A7%B0%E5%AF%B9%E7%85%A7")
 
-    table_data = [[cell.text for cell in row("td")]
-                  for row in BeautifulSoup(res2)("tr")]
-    jsjs = json.load(OrderedDict(table_data))
-    pass
-
-
-def add_item(name, cn_name, web_name, price=0xFFFFFFFF):
-    wanna_item.append({"name": name, "cn_name": cn_name.decode('utf-8'), "web_name": web_name, "min_price": 0xFFFFFFFF,"wanna_price":price})
-#def add_item(name, cn_name, web_name):
+# def add_item(name, cn_name, web_name):
 #    wanna_item.append({"name": name, "cn_name": cn_name.decode('utf-8'), "web_name": web_name, "min_price": 0xFFFFFFFF,"wanna_price":0xFFFFFFFF})
 
 
 def set_wanna_dict():
-# 【锯刃】【漂移接触】 切割
-    add_item("Maiming Strike", "致残突击", "maiming_strike",400) #370 #390
-    add_item("Primed Pressure Point","压迫点 Prime","primed_pressure_point",90)
-    add_item("VOLTAIC STRIKE", "伏打电能", "voltaic_strike",90)
-#    add_item("Buzz Kill", "败兴虐杀", "buzz_kill") #14
-    add_item("Secura Lecta", "保障 勒克塔", "secura_lecta",50)
-#    add_item("Drifting Contact", "漂移接触", "drifting_contact",20) #17
-#    add_item("Virulent Scourge", "剧毒灾害", "virulent_scourge") #5p
-#    add_item("Primed Reach","剑风 Prime","primed_reach",90)#80
-#    add_item("Jagged Edge","锯刃","jagged_edge",4) #3
+    # 【锯刃】【漂移接触】 切割
+    add_item("Jagged Edge")
+    add_item("Maiming Strike", "致残突击", "maiming_strike", 400)  # 370 #390
+    add_item("Primed Pressure Point", "压迫点 Prime", "primed_pressure_point", 90)
+    add_item("VOLTAIC STRIKE", "伏打电能", "voltaic_strike", 90)
+    #    add_item("Buzz Kill", "败兴虐杀", "buzz_kill") #14
+    add_item("Secura Lecta", "保障 勒克塔", "secura_lecta", 50)
+    #    add_item("Drifting Contact", "漂移接触", "drifting_contact",20) #17
+    #    add_item("Virulent Scourge", "剧毒灾害", "virulent_scourge") #5p
+    #    add_item("Primed Reach","剑风 Prime","primed_reach",90)#80
+    #    add_item("Jagged Edge","锯刃","jagged_edge",4) #3
     pass
 
 
 if __name__ == '__main__':
+    get_cn_name()
+    get_url_name()
     set_wanna_dict()
     while True:
         for item in wanna_item:
             find_good_price(item)
-            
-            
-            
-            
-            
-            
-            
-            
+
     # wanna_item["maiming_strike"]=u"致残突击"
 #    get_name()
 # 【败兴虐杀】【锯刃】【漂移接触】
